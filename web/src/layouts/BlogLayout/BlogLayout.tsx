69f9889f2db7bf82e8a62a8b8641fcb4087257ec
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-15 00:04:54 +0200

    feat(layout): Add a link to the home page in blog layout header title

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -9,7 +9,11 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
     <>
       <header className='bg-sky-800 p-4 text-zinc-100'>
         <section className='container mx-auto flex items-center justify-between'>
-          <h1 className='text-xl'>Redwood Blog</h1>
+          <h1 className='text-3xl font-bold text-sky-200'>
+            <Link to={routes.home()} className='hover:text-sky-100'>
+              Redwood Blog
+            </Link>
+          </h1>
           <nav className='flex space-x-4'>
             <Link to={routes.home()}>Home</Link>
             <Link to={routes.about()}>About</Link>
