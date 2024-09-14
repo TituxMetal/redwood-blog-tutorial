3ad4ce99652709504896145d1c66fb7bee8e924e
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-15 00:28:20 +0200

    feat(article): Add article cell to fetch and display a single post

---
+++ web/src/components/ArticleCell/ArticleCell.tsx
@@ -0,0 +1,31 @@
+import type { FindArticleQuery, FindArticleQueryVariables } from 'types/graphql'
+
+import type { CellFailureProps, CellSuccessProps, TypedDocumentNode } from '@redwoodjs/web'
+
+import Article from 'src/components/Article/Article'
+
+export const QUERY: TypedDocumentNode<FindArticleQuery, FindArticleQueryVariables> = gql`
+  query FindArticleQuery($id: String!) {
+    article: post(id: $id) {
+      id
+      body
+      title
+      updatedAt
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps<FindArticleQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  article
+}: CellSuccessProps<FindArticleQuery, FindArticleQueryVariables>) => {
+  return <Article item={article} />
+}
