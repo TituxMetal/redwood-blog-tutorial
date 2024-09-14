9550e930f43c3cf1169ca94c1523a205e30885a0
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-14 10:40:17 +0200

    Add the home page

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Redwood Blog" description="Redwood Blog" />
+
+      <p>This is the home page</p>
+    </>
+  )
+}
+
+export default HomePage
