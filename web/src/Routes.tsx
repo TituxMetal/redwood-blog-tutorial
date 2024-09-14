6e06901797171ae7d8b95e71c0217366eee1cce1
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-14 20:51:35 +0200

    feat(post): Create routes wrapped in the Set component with the layout as ScaffoldLayout in web/src/Routes.tsx

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -10,13 +10,20 @@
 import { Route, Router, Set } from '@redwoodjs/router'
 
 import BlogLayout from 'src/layouts/BlogLayout'
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={ScaffoldLayout} title='Posts' titleTo='posts' buttonLabel='New Post' buttonTo='newPost'>
+        <Route path='/posts/new' page={PostNewPostPage} name='newPost' />
+        <Route path='/posts/{id}/edit' page={PostEditPostPage} name='editPost' />
+        <Route path='/posts/{id}' page={PostPostPage} name='post' />
+        <Route path='/posts' page={PostPostsPage} name='posts' />
+      </Set>
       <Set wrap={BlogLayout}>
-        <Route path="/about" page={AboutPage} name="about" />
-        <Route path="/" page={HomePage} name="home" />
+        <Route path='/about' page={AboutPage} name='about' />
+        <Route path='/' page={HomePage} name='home' />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
