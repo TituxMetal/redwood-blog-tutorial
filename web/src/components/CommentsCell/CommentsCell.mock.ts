4a09c61356844e002ab2b350153cde3cb7620ba5
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-16 20:45:52 +0200

    feat(comments): Add the comments cell component, mock, stories and test files

---
+++ web/src/components/CommentsCell/CommentsCell.mock.ts
@@ -0,0 +1,23 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  comments: [
+    {
+      id: '42',
+      name: 'John Doe',
+      body: 'This is a comment',
+      createdAt: '2024-01-31T13:37:42Z'
+    },
+    {
+      id: '43',
+      name: 'Jane Doe',
+      body: 'This is another comment',
+      createdAt: '2024-08-18T13:37:42Z'
+    },
+    {
+      id: '44',
+      name: 'John Smith',
+      body: 'This is a third comment',
+      createdAt: '2024-04-27T13:37:42Z'
+    }
+  ]
+})
