771cd39ab0ea24615485743b0833171ea9cb43be
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-16 11:23:31 +0200

    feat(auth): Update the blog layout component to be able to show if authenticated or not and add a logout button

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,12 +1,18 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link, NavLink, routes } from '@redwoodjs/router'
+import { Toaster } from '@redwoodjs/web/toast'
+
+import { useAuth } from 'src/auth'
 
 type BlogLayoutProps = {
   children?: React.ReactNode
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
+  const { isAuthenticated, logOut } = useAuth()
+
   return (
     <>
+      <Toaster />
       <header className='bg-sky-800 p-4 text-zinc-100'>
         <section className='container mx-auto flex items-center justify-between'>
           <h1 className='text-3xl font-bold text-sky-200'>
@@ -14,10 +20,37 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
               Redwood Blog
             </Link>
           </h1>
-          <nav className='flex space-x-4'>
-            <Link to={routes.home()}>Home</Link>
-            <Link to={routes.about()}>About</Link>
-            <Link to={routes.contact()}>Contact</Link>
+          <nav className='flex space-x-4 text-lg'>
+            <NavLink
+              to={routes.home()}
+              activeClassName='text-sky-200'
+              className='text-sky-300 hover:text-sky-400'
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to={routes.about()}
+              activeClassName='text-sky-200'
+              className='text-sky-300 hover:text-sky-400'
+            >
+              About
+            </NavLink>
+            <NavLink
+              to={routes.contact()}
+              activeClassName='text-sky-200'
+              className='text-sky-300 hover:text-sky-400'
+            >
+              Contact
+            </NavLink>
+            {isAuthenticated ? (
+              <button type='button' onClick={logOut} className='text-sky-300 hover:text-sky-400'>
+                Logout
+              </button>
+            ) : (
+              <Link to={routes.login()} className='text-sky-300 hover:text-sky-400'>
+                Login
+              </Link>
+            )}
           </nav>
         </section>
       </header>
