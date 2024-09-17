2d3e99e5b63891d023e277468fc322a100a70bed
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-17 09:32:05 +0200

    feat(comments): Add comments sdl

---
+++ api/src/graphql/comments.sdl.ts
@@ -0,0 +1,35 @@
+export const schema = gql`
+  type Comment {
+    id: String!
+    name: String!
+    body: String!
+    post: Post!
+    postId: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type Query {
+    comments: [Comment!]! @skipAuth
+  }
+
+  input CreateCommentInput {
+    name: String!
+    body: String!
+    postId: String!
+  }
+
+  input UpdateCommentInput {
+    name: String
+    body: String
+    postId: String
+  }
+
+  type Mutation {
+    createComment(input: CreateCommentInput!): Comment! @skipAuth
+  }
+
+  type Mutation {
+    deleteComment(id: String!): Comment! @requireAuth
+  }
+`
