80d16c0ea269451be4805d166f2873b410f0f7e6
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-18 08:25:43 +0200

    feat(rbac): Add the userId field in comments cellcomponent and mock

--- web/src/components/CommentsCell/CommentsCell.mock.ts
+++ web/src/components/CommentsCell/CommentsCell.mock.ts
@@ -5,18 +5,21 @@ export const standard = (/* vars, { ctx, req } */) => ({
       id: '42',
       name: 'John Doe',
       body: 'This is a comment',
+      postId: '1',
       createdAt: '2024-01-31T13:37:42Z'
     },
     {
       id: '43',
       name: 'Jane Doe',
       body: 'This is another comment',
+      postId: '1',
       createdAt: '2024-08-18T13:37:42Z'
     },
     {
       id: '44',
       name: 'John Smith',
       body: 'This is a third comment',
+      postId: '1',
       createdAt: '2024-04-27T13:37:42Z'
     }
   ]
