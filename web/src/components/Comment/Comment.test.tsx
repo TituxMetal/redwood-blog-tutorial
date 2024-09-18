06fba1c2b965a3e47d94fe088050937e2d014228
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-18 08:32:43 +0200

    feat(rbac): Add tests to ensure that only a moderator user can delete a comment

--- web/src/components/Comment/Comment.test.tsx
+++ web/src/components/Comment/Comment.test.tsx
@@ -1,25 +1,38 @@
-import { render, screen } from '@redwoodjs/testing/web'
+import { render, screen, waitFor } from '@redwoodjs/testing/web'
 
 import Comment from './Comment'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//    https://redwoodjs.com/docs/testing#testing-components
+const COMMENT = {
+  name: 'John Doe',
+  body: 'This is a comment',
+  createdAt: '2024-02-14T13:37:42Z',
+  id: '1',
+  postId: '1'
+}
 
 describe('Comment', () => {
   it('renders successfully', () => {
-    const comment = {
-      name: 'John Doe',
-      body: 'This is a comment',
-      createdAt: '2024-02-14T13:37:42Z'
-    }
+    render(<Comment comment={COMMENT} />)
 
-    render(<Comment comment={comment} />)
-
-    expect(screen.getByText(comment.name)).toBeInTheDocument()
-    expect(screen.getByText(comment.body)).toBeInTheDocument()
+    expect(screen.getByText(COMMENT.name)).toBeInTheDocument()
+    expect(screen.getByText(COMMENT.body)).toBeInTheDocument()
     const dateExpect = screen.getByText('February 14, 2024 at 13:37')
     expect(dateExpect).toBeInTheDocument()
     expect(dateExpect.nodeName).toEqual('TIME')
-    expect(dateExpect).toHaveAttribute('datetime', comment.createdAt)
+    expect(dateExpect).toHaveAttribute('datetime', COMMENT.createdAt)
+  })
+
+  it('does not render a delete button if user is logged out', async () => {
+    render(<Comment comment={COMMENT} />)
+
+    expect(screen.queryByText('Delete')).not.toBeInTheDocument()
+  })
+
+  it('renders a delete button if user is a moderator', async () => {
+    mockCurrentUser({ id: '1', email: 'moderator@example.com', roles: 'moderator' })
+
+    render(<Comment comment={COMMENT} />)
+
+    await waitFor(() => expect(screen.getByText('Delete')).toBeInTheDocument())
   })
 })
