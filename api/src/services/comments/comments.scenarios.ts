370874fe91ed382848e814c5a47c97b3f86589a7
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-20 00:18:01 +0200

    fix(comments): Update the comment scenario data to include a user in the post. This makes all tests passes

--- api/src/services/comments/comments.scenarios.ts
+++ api/src/services/comments/comments.scenarios.ts
@@ -11,7 +11,15 @@ export const standard = defineScenario<Prisma.CommentCreateArgs>({
         post: {
           create: {
             title: 'Redwood Leaves',
-            body: 'The quick brown fox jumped over the lazy dog.'
+            body: 'The quick brown fox jumped over the lazy dog.',
+            user: {
+              create: {
+                name: 'John Doe',
+                email: 'john@doe.com',
+                hashedPassword: '1234567890',
+                salt: '1234567890'
+              }
+            }
           }
         }
       }
@@ -23,7 +31,15 @@ export const standard = defineScenario<Prisma.CommentCreateArgs>({
         post: {
           create: {
             title: 'Tesla Model S',
-            body: 'The five boxing wizards jump quickly.'
+            body: 'The five boxing wizards jump quickly.',
+            user: {
+              create: {
+                name: 'Jane Doe',
+                email: 'jane@doe.com',
+                hashedPassword: '1234567890',
+                salt: '1234567890'
+              }
+            }
           }
         }
       }
@@ -36,7 +52,15 @@ export const postOnly = defineScenario<Prisma.PostCreateArgs>({
     bark: {
       data: {
         title: 'Bark',
-        body: `A tree's bark is worse than its bite.`
+        body: `A tree's bark is worse than its bite.`,
+        user: {
+          create: {
+            name: 'Hugo Doe',
+            email: 'hugo@doe.com',
+            hashedPassword: '1234567890',
+            salt: '1234567890'
+          }
+        }
       }
     }
   }
